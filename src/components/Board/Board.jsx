8a3401798a6bdafe6ce556af58365ce42e3676ca
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-01 14:56:20 +0300

    half-implemented Board (half click handler, Card rendering)

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import classNames from "classnames";
 
 import Card from "@components/Card";
@@ -8,36 +8,43 @@ import "./Board.css";
 
 const rootClassName = "board";
 
-function Board({ className }) {
+function Board({ className, cards: _cards }) {
   const rootClass = classNames(rootClassName, className);
+
+  const [cards, setCards] = useState(_cards);
+  const [round, setRound] = useState(1);
+
+  const handleCardClick = useCallback(({ id }) => {
+    if (!id) {
+      return;
+    }
+
+    console.log(">>>", id);
+
+    setRound((round) => round + 1);
+  }, []);
+
+  useEffect(() => {
+    setCards(_cards);
+  }, [_cards]);
 
   return (
     <div data-testid={testIds.board} className={rootClass}>
-      <h1 className={`${rootClassName}__title`}>Round 1</h1>
+      <h1 className={`${rootClassName}__title`}>Round {round}</h1>
       <div className={`${rootClassName}__cards-container`}>
-        <Card className={`${rootClassName}__card`} state={"GUESSED"}>
-          <div style={{ background: "red", width: "100%", height: "100%" }} />
-        </Card>
-        <Card className={`${rootClassName}__card`} state={"OPENED"}>
-          <div style={{ background: "red", width: "100%", height: "100%" }} />
-        </Card>
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
-        <Card className={`${rootClassName}__card`} />
+        {cards.map(({ state, viewValue, id }) => (
+          <Card
+            id={id}
+            key={id}
+            className={`${rootClassName}__card`}
+            state={state}
+            onClick={handleCardClick}
+          >
+            <div
+              style={{ background: viewValue, width: "100%", height: "100%" }}
+            />
+          </Card>
+        ))}
       </div>
     </div>
   );
