f541069e542933046c35aaedad00b4d6d7d14614
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-01 14:55:09 +0300

    implemented data getting

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import Board from "@components/Board";
 import { testIds } from "@constants";
+import getData from "@utils/getData";
 
 import "./App.css";
+
+const data = getData();
 
 function App() {
   return (
     <div data-testid={testIds.app}>
-      <Board />
+      <Board cards={data} />
     </div>
   );
 }
---
+++ src/utils/getData.js
@@ -0,0 +1,86 @@
+const data = [
+  {
+    id: "1",
+    viewValue: "red",
+    modelValue: "red",
+  },
+  {
+    id: "2",
+    viewValue: "red",
+    modelValue: "red",
+  },
+  {
+    id: "3",
+    viewValue: "green",
+    modelValue: "green",
+  },
+  {
+    id: "4",
+    viewValue: "green",
+    modelValue: "green",
+  },
+  {
+    id: "5",
+    viewValue: "yellow",
+    modelValue: "yellow",
+  },
+  {
+    id: "6",
+    viewValue: "yellow",
+    modelValue: "yellow",
+  },
+  {
+    id: "7",
+    viewValue: "pink",
+    modelValue: "pink",
+  },
+  {
+    id: "8",
+    viewValue: "pink",
+    modelValue: "pink",
+  },
+  {
+    id: "9",
+    viewValue: "orange",
+    modelValue: "orange",
+  },
+  {
+    id: "10",
+    viewValue: "orange",
+    modelValue: "orange",
+  },
+  {
+    id: "11",
+    viewValue: "purple",
+    modelValue: "purple",
+  },
+  {
+    id: "12",
+    viewValue: "purple",
+    modelValue: "purple",
+  },
+  {
+    id: "13",
+    viewValue: "black",
+    modelValue: "black",
+  },
+  {
+    id: "14",
+    viewValue: "black",
+    modelValue: "black",
+  },
+  {
+    id: "15",
+    viewValue: "gray",
+    modelValue: "gray",
+  },
+  {
+    id: "16",
+    viewValue: "gray",
+    modelValue: "gray",
+  },
+];
+
+export default function getData() {
+  return data;
+}
