da14a87d5381e0be5128f7e8acf42ce83dbee7d3
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-02 19:53:00 +0300

    added tests

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -2,7 +2,11 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Board from ".";
+import getData from "@utils/getData";
 import { testIds } from "@constants";
+import { shuffle } from "@helpers";
+
+const data = getData();
 
 describe("testing Board component", () => {
   it("renders without error", () => {
@@ -18,5 +22,33 @@ describe("testing Board component", () => {
     const root = getByTestId(testIds.board);
 
     expect(root.classList.contains(testClassname)).toBe(true);
+  });
+
+  it("handles prop cards and game", () => {
+    const game = shuffle(Object.keys(data));
+    const { getAllByTestId } = render(<Board cards={data} game={game} />);
+    const cards = getAllByTestId(testIds.card);
+
+    expect(cards[10].getAttribute("id")).toBe(game[10]);
+  });
+
+  it("handles prop renderCardContent", () => {
+    const renderCardContent = jest.fn();
+    const game = shuffle(Object.keys(data));
+    render(
+      <Board cards={data} game={game} renderCardContent={renderCardContent} />
+    );
+
+    expect(renderCardContent.mock.calls.length).toBe(game.length);
+  });
+
+  it("opens clicked card", () => {
+    const game = shuffle(Object.keys(data));
+    const { getAllByTestId } = render(<Board cards={data} game={game} />);
+    const cards = getAllByTestId(testIds.card);
+
+    cards[0].click();
+
+    expect(cards[0].classList.contains("card--opened")).toBe(true);
   });
 });
--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -19,4 +19,25 @@ describe("testing Card component", () => {
 
     expect(root.classList.contains(testClassname)).toBe(true);
   });
+
+  it("handles prop onClick", () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(<Card onClick={onClick} />);
+    const root = getByTestId(testIds.card);
+
+    root.click();
+
+    expect(onClick.mock.calls.length).toBe(1);
+  });
+
+  it("renders content", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Card onClick={onClick}>
+        <div>TEST_CHILD</div>
+      </Card>
+    );
+
+    expect(getByText("TEST_CHILD")).toBeTruthy();
+  });
 });
