6e312c6585acc427b49553ae76cb48cb35795116
kpodmasko <podmasko.konstantin@gmail.com>
2020-06-02 21:49:40 +0300

    Updates

---
+++ precache-manifest.ffc67d00ed3bd2ca6eb1cb9dc39a51bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f715222d85aee005fa3f69e19d8a8274",
+    "url": "/cdg-tiles-board/index.html"
+  },
+  {
+    "revision": "24ffc61da63b523884f4",
+    "url": "/cdg-tiles-board/static/css/main.368213b7.chunk.css"
+  },
+  {
+    "revision": "d58b067e632e11e138a0",
+    "url": "/cdg-tiles-board/static/js/2.7f2e5cc2.chunk.js"
+  },
+  {
+    "revision": "a11db92bd7761d7062b193d8d5a33c35",
+    "url": "/cdg-tiles-board/static/js/2.7f2e5cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ffc61da63b523884f4",
+    "url": "/cdg-tiles-board/static/js/main.fea1144d.chunk.js"
+  },
+  {
+    "revision": "9e7183195cf22d50186f",
+    "url": "/cdg-tiles-board/static/js/runtime-main.a99e5b7f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cdg-tiles-board/precache-manifest.ffc67d00ed3bd2ca6eb1cb9dc39a51bd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdg-tiles-board/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
